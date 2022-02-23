8bc3a0a6961bd719e5a28587311098e66f55fe96
Matheus Camba <mhscamba@gmail.com>
2022-02-23 16:02:02 -0300

    mod: app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import Usuarios from './controllers/usuarios.js';
 
 const app = express();
 // const port = process.env.PORT /PARA ACESSAR O AMBIENTE
@@ -9,11 +10,13 @@ app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`)
 })
 
+Usuarios.usuarios(app);
+
 app.get('/', (req, res)=>{  //a / é a pasta raiz
     res.send('<h1>Funciona</h1>')
 } ) 
 
-app.get('/usuario', (req, res)=>{
+app.get('/usuarios', (req, res)=>{
     res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
 })
 
@@ -29,10 +32,10 @@ app.get('/feito', (req, res)=>{
     res.send('Rota ativada com GET e recurso feito: valores de feito devem ser retornados')
 })
 
+
+//PEGANDO A RESPOSTA E APLICANDO O SEND POR ARRAY
 // const listaEntidades = ['usuario','afazer', 'fazendo', 'feito']
-
 // function criaRotas(listaRotas){
-
 //     for(const entidade of listaRotas) {
 //     app.get(`/${entidade}`, (req, res) => {
 //         res.send(`Rota ativada com GET e recurso ${entidade}: valores de ${entidade} devem ser retornados`)
