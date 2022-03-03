eeb157238ecbd5eef4c1033cecbc8ef69920fcd7
Matheus Camba <mhscamba@gmail.com>
2022-03-03 16:01:44 -0300

    app, usuarios e package.json modified

--- src/app.js
+++ src/app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import Usuarios from './controllers/usuarios.js';
+import Tarefas from './controllers/tarefas.js';
 
 const app = express();
 // const port = process.env.PORT /PARA ACESSAR O AMBIENTE
 
 const port = process.env.PORT || 3000;
 
+//transforma o body em json antes de chegar na rota
+app.use(express.json()) //seria o bodyparse
+//podemos usar um MIDDLEWARE da mesma forma. 
+
 app.listen(port, ()=>{
     console.log(`Servidor rodando na porta ${port}`)
 })
 
 Usuarios.usuarios(app);
+Tarefas.tarefas(app);
 
 app.get('/', (req, res)=>{  //a / é a pasta raiz
     res.send('<h1>Funciona</h1>')
@@ -20,17 +26,17 @@ app.get('/usuarios', (req, res)=>{
     res.send('Rota ativada com GET e recurso usuario: valores de usuario devem ser retornados')
 })
 
-app.get('/afazer', (req, res)=>{
-    res.send('Rota ativada com GET e recurso A fazer: valores de afazer devem ser retornados')
+app.get('/tarefas', (req, res)=>{
+    res.send('Rota ativada com GET e recurso tarefas: valores de tarefas devem ser retornados')
 })
 
-app.get('/fazendo', (req, res)=>{
-    res.send('Rota ativada com GET e recurso fazendo: valores de fazendo devem ser retornados')
-})
+// app.get('/fazendo', (req, res)=>{
+//     res.send('Rota ativada com GET e recurso fazendo: valores de fazendo devem ser retornados')
+// })
 
-app.get('/feito', (req, res)=>{
-    res.send('Rota ativada com GET e recurso feito: valores de feito devem ser retornados')
-})
+// app.get('/feito', (req, res)=>{
+//     res.send('Rota ativada com GET e recurso feito: valores de feito devem ser retornados')
+// })
 
 
 //PEGANDO A RESPOSTA E APLICANDO O SEND POR ARRAY
--- src/controllers/usuarios.js
+++ src/controllers/usuarios.js
@@ -1,9 +1,21 @@
+import { request } from "express";
+import { database } from "../../infra/database.js";
 //VERSAO EM CLASS
 export class Usuarios{
     static usuarios(app){
         app.get('/usuarios', (req, res)=>{
-            res.send(`<h1>Rota Usuários via método GET.</h1>`)
+            res.status(200).json(database)
         })
+        app.post('/usuarios', (req, res)=>{
+            database.push(req.body)
+            res.status(201).json(database)
+            // res.send(`Rota POST de tarefa ativada: tarefa adiciona ao banco de dados`)
+        })
+        // app.post('/afazer', (req, res)=>{
+        //     // res.status(201).json(database)
+        //     const body = request.body;
+        //     res.send(`${body}`)
+        // })
     }
 }
 
